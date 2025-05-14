64d38a1a954d4ff1930b8f38ab22e7b506d0f803
Alex <slayer1649@gmail.com>
2025-05-14 18:03:58 +0200

    -

--- src/main/java/com/aldrin/foodfam/repository/IngredientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.aldrin.foodfam.repository;
-
-import com.aldrin.foodfam.model.Ingredient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IngredientRepository extends JpaRepository<Ingredient,Long> {
-}
